e2128c7a64d2d9095e7ea1bdff08338cd61af21e
Aleksandra Bielawa <110456391+abielawa@users.noreply.github.com>
2024-04-17 14:43:24 +0200

    Feat/helm chart setup (#4)
    * feat: add helm chart deplyoment with custom values and namespace creation
    
    * chore: update .gitignore
    
    * Update namespace variable assignment

--- deploy/observability/index.js
+++ deploy/observability/index.js
@@ -2,22 +2,28 @@
 import * as pulumi from '@pulumi/pulumi';
 import * as k8s from '@pulumi/kubernetes';
 import * as config from './src/config.js'
-import {installHelmChart} from './src/chart_install.js';
+import { installHelmChart } from './src/chart_install.js';
 
 
-// TODO: create function which create namespace
+const createNamespace = (name) => {
+    return new k8s.core.v1.Namespace(name, {
+        metadata: {
+            name: name,
+        },
+    });
+}
 
 export default async () => {
 
-    // TODO: invoke namespace installation function
+    const observabilityNamespace = createNamespace('observability');
+    const namespace = observabilityNamespace.metadata.name
 
     // TODO: install opentelemtry operator
-
     // TODO: invoke helm chart installation. Change passed values
     let prometheusHelmChart;
 
     if (config.installPrometheus) {
-        prometheusHelmChart = installHelmChart('promethues', 'X.Y.Z', 'namespace', 'path to values file')
+        prometheusHelmChart = installHelmChart('prometheus', '25.19.1', namespace, './charts_values/prometheus_values.yaml', 'https://prometheus-community.github.io/helm-charts')
     }
     //
     // TODO: further installed chart processing if required
--- deploy/observability/src/chart_install.js
+++ deploy/observability/src/chart_install.js
@@ -1,16 +1,29 @@
 import * as pulumi from '@pulumi/pulumi';
 import * as k8s from '@pulumi/kubernetes';
+import fs from 'fs';
+import yaml from 'js-yaml';
 
 // use to install Helm Charts
 // TODO: override default values
-export const installHelmChart = (chartName = 'default', chartVersion = 'default', chartNamespace = 'default', chartValuesPath = ' default') => {
+export const installHelmChart = (chartName = 'default', chartVersion = 'default', chartNamespace = 'default', chartValuesPath = ' ./default-values.yaml', repoUrl = 'default') => {
 
-    // TODO: preload yaml files from chartValuesPath
+    let values = {};
+
+    try {
+        const loadValues = fs.readFileSync(chartValuesPath, 'utf8');
+        values = yaml.load(loadValues);
+    }
+    catch (e) {
+        console.log("Error reading file:", e);
+    }
 
     return new k8s.helm.v3.Chart(chartName, {
         chart: chartName,
         version: chartVersion,
         namespace: chartNamespace,
-        values: {} // TODO: add code load values from yaml files. Using YAML files for easy modification
+        values: values,
+        fetchOpts: {
+            repo: repoUrl,
+        },
     });
 }
