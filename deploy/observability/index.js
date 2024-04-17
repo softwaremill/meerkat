4225e8d7eb652c1fadf83ab26acfd7024a40adfa
abielawa <aleksandra.bielawa@softwaremill.com>
2024-04-17 11:36:15 +0200

    feat: add helm chart deplyoment with custom values and namespace creation

--- deploy/observability/index.js
+++ deploy/observability/index.js
@@ -2,14 +2,20 @@
 import * as pulumi from '@pulumi/pulumi';
 import * as k8s from '@pulumi/kubernetes';
 import * as config from './src/config.js'
-import {installHelmChart} from './src/chart_install.js';
+import { installHelmChart } from './src/chart_install.js';
 
 
-// TODO: create function which create namespace
+const createNamespace = (name) => {
+    return new k8s.core.v1.Namespace(name, {
+        metadata: {
+            name: name,
+        },
+    });
+}
 
 export default async () => {
 
-    // TODO: invoke namespace installation function
+    const observabilityNamespace = createNamespace('observability');
 
     // TODO: install opentelemtry operator
 
@@ -17,7 +23,7 @@ export default async () => {
     let prometheusHelmChart;
 
     if (config.installPrometheus) {
-        prometheusHelmChart = installHelmChart('promethues', 'X.Y.Z', 'namespace', 'path to values file')
+        prometheusHelmChart = installHelmChart('prometheus', '25.19.1', 'observability', './charts_values/prometheus_values.yaml', 'https://prometheus-community.github.io/helm-charts')
     }
     //
     // TODO: further installed chart processing if required
