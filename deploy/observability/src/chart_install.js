85ec3a139cb2dc8abd14454335c771e2153c9063
abielawa <aleksandra.bielawa@softwaremill.com>
2024-04-26 12:52:59 +0200

    Refactor observability deployment to use HelmRelease class

--- deploy/observability/src/chart_install.js
+++ deploy/observability/src/chart_install.js
@@ -3,28 +3,40 @@ import * as k8s from '@pulumi/kubernetes';
 import fs from 'fs';
 import yaml from 'js-yaml';
 
-// use to install Helm Charts
-// TODO: override default values
-export const installHelmRelease = (chartName = 'default', chartVersion = 'default', chartNamespace = 'default', chartValuesPath = ' ./default-values.yaml', repoUrl = 'default') => {
+export class HelmRelease extends pulumi.ComponentResource {
 
-    let values = {};
+    _readValuesFile(path) {
+        let values = {};
+        try {
+            const loadValues = fs.readFileSync(path, 'utf8');
+            values = yaml.load(loadValues);
+        }
+        catch (e) {
+            console.log("Error reading file:", e);
+        }
 
-    try {
-        const loadValues = fs.readFileSync(chartValuesPath, 'utf8');
-        values = yaml.load(loadValues);
+        return values;
     }
-    catch (e) {
-        console.log("Error reading file:", e);
-    }
 
-    return new k8s.helm.v3.Release(chartName, {
-        chart: chartName,
-        name: chartName,
-        version: chartVersion,
-        namespace: chartNamespace,
-        values: values,
-        repositoryOpts: {
-            repo: repoUrl,
+    constructor(releaseName, helmConfig, opts) {
+        super("meerkat:HelmRelease", releaseName, helmConfig, opts);
+
+        let values = this._readValuesFile(helmConfig.chartValuesPath);
+
+        let helmRelease = new k8s.helm.v3.Release(releaseName, {
+            chart: helmConfig.chartName,
+            name: helmConfig.chartName,
+            version: helmConfig.chartVersion,
+            namespace: helmConfig.chartNamespace,
+            values: values,
+            repositoryOpts: {
+                repo: helmConfig.repoUrl,
+            }
         },
-    });
+            { parent: this },
+        );
+
+        this.chartName = helmRelease.name;
+        this.registerOutputs();
+    }
 }
