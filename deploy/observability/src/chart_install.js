23919c2547eb8ee3cb4c094abf8c72bc85a9cd3e
Aleksandra Bielawa <110456391+abielawa@users.noreply.github.com>
2024-04-29 13:44:14 +0200

    chore: refactor passing values in helm release (#13)
    * Refactor passing values in helm release
    
    * chore: syntax fix
    
    * chore: remove extra import
    
    * fix: rename the variable

--- deploy/observability/src/chart_install.js
+++ deploy/observability/src/chart_install.js
@@ -1,34 +1,17 @@
-import * as pulumi from '@pulumi/pulumi';
-import * as k8s from '@pulumi/kubernetes';
-import fs from 'fs';
-import yaml from 'js-yaml';
+import * as pulumi from "@pulumi/pulumi";
+import * as k8s from "@pulumi/kubernetes";
 
 export class HelmRelease extends pulumi.ComponentResource {
 
-    _readValuesFile(path) {
-        let values = {};
-        try {
-            const loadValues = fs.readFileSync(path, 'utf8');
-            values = yaml.load(loadValues);
-        }
-        catch (e) {
-            console.log("Error reading file:", e);
-        }
-
-        return values;
-    }
-
     constructor(releaseName, helmConfig, opts) {
         super("meerkat:HelmRelease", releaseName, helmConfig, opts);
-
-        let values = this._readValuesFile(helmConfig.chartValuesPath);
 
         let helmRelease = new k8s.helm.v3.Release(releaseName, {
             chart: helmConfig.chartName,
             name: helmConfig.chartName,
             version: helmConfig.chartVersion,
             namespace: helmConfig.chartNamespace,
-            values: values,
+            valueYamlFiles: [new pulumi.asset.FileAsset(helmConfig.chartValuesPath)],
             repositoryOpts: {
                 repo: helmConfig.chartRepositoryUrl,
             }
