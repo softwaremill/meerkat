22e47dcd214d6ee26ffd6b8e5cf1abf6f734f254
Hubert Klusek <hubert.klusek@gmail.com>
2024-05-05 19:28:29 +0200

    feature/SO-24 change ESlint config

--- deploy/observability/eslint.config.js
+++ deploy/observability/eslint.config.js
@@ -1,8 +1,10 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import js from "@eslint/js";
 
 export default [
-    { languageOptions: { globals: globals.browser } },
-    pluginJs.configs.recommended,
+    js.configs.recommended,
+    {
+        rules: {
+            "no-unused-vars": "warn"
+        }
+    }
 ];
